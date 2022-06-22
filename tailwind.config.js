42ea8aa1b110de7f5608fecf550ab7434d4536ab
Greeshma Medam <greeshma.medam@gmail.com>
2022-06-22 20:13:02 +0530

    Almosstt Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,18 @@ module.exports = {
   content: ["./index.html", "./src/*.js"],
   theme: {
     extend: {
-      fontFamily:{
-        body: ["Lato", "Helvetica", "sans"],
-        highlight: ["Playfair Display", "serif"]
-      }
+      fontFamily: {
+        'body': ["Lato", "Helvetica", "sans"],
+        'highlight': ["Playfair Display", "Prata", "serif"],
+      },
+      colors: {
+        'Indiblue': "#5E6EFF",
+        'PotRed': "#F21F58",
+        'PaleYellow': "#FFFA80",
+        'DarkBg': "#111111",
+        'CardBg': '#1E1E1E',
+      },
     },
   },
   plugins: [],
-}
+};
